9c9bbbfe3cb9d15ec5ff3227eeecca238dbb0e86
minot <mohthegoat1@proton.me>
2025-05-01 16:32:15 +0100

    move to moderationg folder

--- Bot/Commands/moderation/ban.js
+++ Bot/Commands/moderation/ban.js
@@ -4,14 +4,24 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
         .setDescription('Bans a member from this guild')
-        .addUserOption(option =>
-            option.setName('user')
+        .addUserOption(option => option
+                .setName('user')
                 .setDescription('User to ban')
                 .setRequired(true)
+        )
+        .addStringOption(option => option
+            .setName('reason')
+            .setDescription('Reason to ban this member')
+        )
+        .addNumberOption(option => option
+            .setName('duration')
+            .setDescription('Durating in hours for messages to delete')
         ),
     async execute(interaction) {
         const user = interaction.options.getUser('user')
         const member = interaction.options.getMember('user')
+        const reason = interaction.options.getString('reason') ?? 'No reason given'
+        const duration = interaction.options.getNumber('duration')*60*60 ?? 0
         if (member) {
             if (!interaction.member.permissions.has("BanMembers")) {
                 const embed = new EmbedBuilder()
@@ -55,7 +65,7 @@ module.exports = {
                 .addFields([
                     {name: '', value: `Banned user <@${user.id}> (${user.id})`}
                 ])
-            interaction.guild.members.ban(`${user.id}`)
+            interaction.guild.members.ban(user.id, {deleteMessageSeconds: duration , reason: `reason: ${reason}, by <@${interaction.user.id}>`})
             await interaction.reply({embeds: [embed]})
         } else {
             const embed = new EmbedBuilder()
@@ -63,7 +73,7 @@ module.exports = {
                 .addFields([
                     {name: '', value: `Banned user <@${user.id}> (${user.id})`}
                 ])
-            interaction.guild.members.ban(`${user.id}`)
+            interaction.guild.members.ban(user.id, {deleteMessageSeconds: duration , reason: `reason: ${reason}, by <@${interaction.user.id}>`})
             await interaction.reply({embeds: [embed]})
         }
     }
