36594b5e038913c4a65a772dfd1bcd9789c41f38
minot <mohthegoat1@proton.me>
2025-05-03 07:28:41 +0100

    mmm

--- Bot/Deploy.js
+++ Bot/Deploy.js
@@ -24,15 +24,12 @@ for (const folder of commandFolders) {
     }
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
-// and deploy your commands!
 (async () => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands },
@@ -40,7 +37,6 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
-        // And of course, make sure you catch and log any errors!
         console.error(error);
     }
 })();
