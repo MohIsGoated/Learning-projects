ca379f453f0646ed2a790cbd53af6d725c0aa0f9
minot <mohthegoat1@proton.me>
2025-05-13 09:36:52 +0100

    add status change cmd

---
+++ Bot/Commands/util/status.js
@@ -0,0 +1,39 @@
+const { ActivityType, EmbedBuilder, SlashCommandBuilder, resolveColor} = require('discord.js')
+const { execute, queryone, queryall, db, exists} = require('../../utils/db')
+const config = require('../../config.json')
+config.footer = config.footer || 'Made with luv ❤️';
+config.footerUrl = config.footerUrl || 'https://cdn.discordapp.com/avatars/1086622488374550649/8901d89d61aad251caf017646932a7d3.webp?size=1024'
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('status')
+        .setDescription(`Set the bot's status`)
+        .addStringOption(option => option
+            .setName('text')
+            .setDescription(`what to set it to`)
+            .setMaxLength(128)
+            .setRequired(true)
+        )
+        .addStringOption(option => option
+            .setName('state')
+            .setDescription('The online state of the bot')
+            .addChoices(
+                {name: 'Online', value: 'online'},
+                {name: 'Do Not Disturb', value: 'dnd'},
+                {name: 'Idle', value: 'idle'},
+                {name: 'Invisible', value: 'invisible'}
+            )
+        ),
+    adminOnly: true,
+    async execute(interaction) {
+        const input = interaction.options.getString('text')
+        const state = interaction.options.getString('state') ?? 'online'
+        interaction.client.user.setPresence({
+            activities: [{
+                name: input,
+                type: ActivityType.Custom
+            }],
+            status: state
+        })
+    }
+}
